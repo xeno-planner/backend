4f73356586b0adae3eb3cc6b88fba4913e2af398
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-19 14:06:38 +0300

    🗑️ remove useless module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ConfigModule.forRoot(), UserModule],
+  imports: [AuthModule, ConfigModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
