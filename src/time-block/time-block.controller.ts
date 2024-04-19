b9a56c3160816780c1e2f1ec8d299cbca1ede258
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-19 18:20:28 +0300

    📝 create all crud endpoint for time blocking

--- src/time-block/time-block.controller.ts
+++ src/time-block/time-block.controller.ts
@@ -1,8 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+
+import { Auth } from '@/auth/decorators/auth.decorator';
+import { CurrentUser } from '@/auth/decorators/user.decorator';
+import { TimeBlockDto } from '@/time-block/dto/time-block.dto';
+import { UpdateOrderDto } from '@/time-block/dto/update-order.dto';
 
 import { TimeBlockService } from './time-block.service';
 
 @Controller('user/time-block')
 export class TimeBlockController {
   constructor(private readonly timeBlockService: TimeBlockService) {}
+
+  @HttpCode(200)
+  @Get()
+  @Auth()
+  async getAll(@CurrentUser('id') userId: string) {
+    return this.timeBlockService.getAll(userId);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post()
+  @Auth()
+  async create(@Body() dto: TimeBlockDto, @CurrentUser('id') userId: string) {
+    return this.timeBlockService.create(dto, userId);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Put(':id')
+  @Auth()
+  async update(
+    @Body() dto: TimeBlockDto,
+    @CurrentUser('id') userId: string,
+    @Param('id') id: string,
+  ) {
+    return this.timeBlockService.update(dto, id, userId);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Put('update-order')
+  @Auth()
+  async updateOrder(@Body() dto: UpdateOrderDto) {
+    return this.timeBlockService.updateOrder(dto.ids);
+  }
+
+  @HttpCode(200)
+  @Delete(':id')
+  @Auth()
+  async delete(@CurrentUser('id') userId: string, @Param('id') id: string) {
+    return this.timeBlockService.delete(id, userId);
+  }
 }
--- src/time-block/time-block.module.ts
+++ src/time-block/time-block.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { PrismaService } from '@/prisma.service';
+
 import { TimeBlockController } from './time-block.controller';
 import { TimeBlockService } from './time-block.service';
 
 @Module({
   controllers: [TimeBlockController],
-  providers: [TimeBlockService],
+  providers: [TimeBlockService, PrismaService],
 })
 export class TimeBlockModule {}
