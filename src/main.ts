280ff5c727239f9c6ded9f7689146c99b4d5fe97
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-19 13:32:55 +0300

    🪴 initial app setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import 'dotenv/config';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [process.env.FRONT_END_HOST],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+
   await app.listen(4242);
 }
 bootstrap();
