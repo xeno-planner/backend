2fff66cd5e942d3e9280ea2b393dc52a589d1dae
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-21 16:35:40 +0300

    🤕 add heading to template

--- src/assets/templates/Verification.template.tsx
+++ src/assets/templates/Verification.template.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import MyButton from './components/MyButton';
 import EmailLayout from '@/assets/templates/components/EmailLayout';
+import { Heading } from '@react-email/components';
 
 const VerificationTemplate = ({ url }: { url: string }) => {
   return (
     <EmailLayout>
+      <Heading as={'h1'}>Подтверждение учетной записи на сайте XENO Planner</Heading>
+
       <MyButton url={url}>
         Подтвердить регистрацию
       </MyButton>
