141d338c76c271101ad59501814f699fbe9d4e01
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-22 09:53:18 +0300

    🚌 connect nodemailer transport

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { PrismaService } from '@/prisma.service';
 import { UserService } from '@/user/user.service';
@@ -6,6 +7,7 @@ import { UserService } from '@/user/user.service';
 import { MailService } from './mail.service';
 
 @Module({
+  imports: [ConfigModule],
   providers: [MailService, UserService, PrismaService],
 })
 export class MailModule {}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,11 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { renderAsync } from '@react-email/components';
+import { Transporter, createTransport } from 'nodemailer';
 
 import { UserService } from '@/user/user.service';
 
+interface SendMailConfig {
+  email: string;
+  subject: string;
+  html?: string;
+}
+
 @Injectable()
 export class MailService {
-  constructor(private readonly userService: UserService) {}
+  private transporter: Transporter;
+
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {
+    // Connection data
+    const host = this.configService.get('MAIL_HOST');
+    const port = Number(this.configService.get('MAIL_PORT')) || 0;
+    const secure = this.configService.get('MAILER_SECURE') === 'true';
+
+    // Auth
+    const user = this.configService.get('MAIL_USER');
+    const password = this.configService.get('MAIL_PASSWORD');
+
+    // From
+    const fromName = this.configService.get('MAIL_FROM_NAME');
+    const fromAddress = this.configService.get('MAIL_FROM_ADDRESS');
+
+    this.transporter = createTransport(
+      {
+        host,
+        port,
+        secure,
+        auth: {
+          user,
+          pass: password,
+        },
+      },
+      {
+        from: {
+          name: fromName,
+          address: fromAddress,
+        },
+      },
+    );
+  }
 
   /**
    * Method that renders react-email components to string html.
@@ -19,6 +63,20 @@ export class MailService {
   }
 
   /**
+   * This method sends email to given address.
+   * @param email
+   * @param subject
+   * @param html
+   */
+  async sendMailTo({ email, subject, html }: SendMailConfig) {
+    await this.transporter.sendMail({
+      to: email,
+      subject,
+      html,
+    });
+  }
+
+  /**
    * Send email to email of user with certain __userId__.
    * @param userId
    * @param params
@@ -27,11 +85,13 @@ export class MailService {
     userId: string,
     ...params: Parameters<typeof renderAsync>
   ) {
-    const { email: emailAddress } = await this.userService.getById(userId);
+    const { email } = await this.userService.getById(userId);
     const html = await this.renderEmail(...params);
 
-    console.log({
-      emailAddress,
+    /** Send email letter to user`s email address. */
+    await this.sendMailTo({
+      email,
+      subject: 'Подтверждение учетной записи',
       html,
     });
   }
