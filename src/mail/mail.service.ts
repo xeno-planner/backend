c83e04b6c610a7c82f8c513536233d6968fc061f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-22 12:05:06 +0300

    🚮 remove mailer service from mail creation

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,5 +1,4 @@
-import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { renderAsync } from '@react-email/components';
 
@@ -16,7 +15,6 @@ export class MailService {
   constructor(
     private readonly userService: UserService,
     private readonly configService: ConfigService,
-    private readonly mailerService: MailerService,
   ) {}
 
   /**
@@ -34,17 +32,11 @@ export class MailService {
    * This method sends email to given address.
    */
   async sendMailTo({ email, subject, html }: SendMailConfig) {
-    await this.mailerService
-      .sendMail({
-        to: email,
-        subject,
-        html,
-      })
-      .catch(err => {
-        throw new UnprocessableEntityException(
-          `Error in mailer work: ${JSON.stringify(err)}`,
-        );
-      });
+    console.log({
+      email,
+      subject,
+      html,
+    });
   }
 
   /**
