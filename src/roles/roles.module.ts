d38dbeedec8696b66aaa69d6ecc0d76e75a95458
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-01-01 17:07:00 +0200

    Revert "🎁 create roles controller"
    This reverts commit a00b8642a46e2509e5bcd6c7a52b7597427822c2.

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -3,11 +3,9 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '@/prisma.service';
 import { UserService } from '@/user/user.service';
 
-import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 
 @Module({
   providers: [RolesService, PrismaService, UserService],
-  controllers: [RolesController],
 })
 export class RolesModule {}
