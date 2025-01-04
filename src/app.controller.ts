e1353e88f2faab5628803f4faafa62809953c66d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-01-04 10:06:34 +0200

    🚮 remove db check from usual request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,18 +9,8 @@ export class AppController {
   @Get()
   @HttpCode(200)
   async helloWorld() {
-    let status = 'running';
-
-    // Get health info
-    const { canConnect: canConnectDb } = await this.appService.getDbStatus();
-
-    // Got some errors
-    if ([!canConnectDb].some(v => v === false)) {
-      status = 'running with errors';
-    }
-
     return {
-      status,
+      status: 'running',
     };
   }
 }
