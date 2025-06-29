c05430107752bfe8748351fc26084391b675c7a2
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-06-29 20:32:30 +0200

    💯 return global prefix

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
     origin: [process.env.FRONT_END_HOST],
