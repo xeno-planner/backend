1aef24be2006da084d140a9af5fb6f7d0338b1b9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-04 21:16:19 +0200

    ✏️ remove lax cookies

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -109,8 +109,7 @@ export class AuthService {
       // true if production
       secure: envMode === 'prod',
       // lax if production
-      // sameSite: envMode === 'prod' ? 'lax' : 'none',
-      sameSite: 'lax',
+      sameSite: envMode === 'prod' ? 'strict' : 'none',
     };
   }
 
