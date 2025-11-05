58ebe94131b427946239c2a9db4918245317977c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-11-05 10:53:59 +0200

    ✍️ implement contract

---
+++ src/contracts/user.contract.ts
@@ -0,0 +1,29 @@
+export interface UserContract<
+  Shape extends UserShape,
+  ExtendedShape extends Shape,
+  UpdateShape extends Shape,
+  AuthDto extends AuthDtoShape,
+  UpdateDto extends UpdateDtoShape,
+> {
+  getById(id: Shape['id']): Promise<ExtendedShape>;
+  getByLogin(login: Shape['login']): Promise<ExtendedShape>;
+  create(dto: AuthDto): Promise<Shape>;
+  update(id: Shape['id'], dto: UpdateDto): Promise<UpdateShape>;
+  delete(id: Shape['id']): Promise<void>;
+}
+
+type UserShape = {
+  id: string;
+  login: string;
+  name?: string;
+  password: string;
+};
+
+type AuthDtoShape = {
+  login: string;
+  password: string;
+};
+
+type UpdateDtoShape = AuthDtoShape & {
+  name: string;
+};
